a79a4bf8e1d6359290083de5e81f9bbac6bbffc0
AkitoTsukahara <tsukahara.akito@gmail.com>
2021-02-14 23:13:29 +0900

    data,methods,v-model

---
+++ pages/action.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <div class="container">{{ first_name }} {{ last_name }}</div>
+        <button @click="changeText()">{{ btnLastName }}</button>
+        <button @click="backText()">{{ BtnFirstName }}</button>
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+ 
+export default Vue.extend({
+    data: () => ({
+        first_name: 'Akito',
+        last_name: 'Tsukahara',
+        btnLastName: 'ChangeText',
+        btnFirstName: 'BackText'
+    }),
+    methods: {
+        changeText() {
+            this.first_name = 'XXXXX'
+        },
+        backText() {
+            this.first_name = 'Akito'
+        }
+    }
+})
+</script>
+ 
+<style></style>
